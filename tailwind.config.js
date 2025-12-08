9c61ddb36da7bab74eebebc21b5616c66ef430c7
placebetsai <placebetsai@gmail.com>
2025-12-07 22:24:33 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
-        neon: '#00f3ff',
-        hot: '#ff0055',
-        dark: '#050505',
+        neon: "#00f3ff",
+        hot: "#ff0055",
+        dark: "#050505"
       },
-    },
+      fontFamily: {
+        mono: ["Courier New", "monospace"]
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
